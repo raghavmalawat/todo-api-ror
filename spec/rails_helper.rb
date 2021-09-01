292780d287833ec42e891e74937e5e82074c75df
Raghav Malawat <raghavmalawat@gmail.com>
2021-09-01 16:18:34 +0530

    Items tests and api's

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,21 +2,23 @@
 require 'database_cleaner'
 require 'factory_bot'
 
-# [...]
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
-# Shoulda::Matchers.configure do |config|
-#   config.integrate do |with|
-#     with.test_framework :rspec
-#     with.library :rails
-#   end
-# end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
-# [...]
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # [...]
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
-
+  config.include RequestSpecHelper, type: :request
+  config.infer_spec_type_from_file_location!
+  
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
